27dab1170427b69d6b8ca6d09c0c77945d0d0af5
Jorge Alejandro González Frías <jorge.gonzalezf@softtek.com>
2020-02-19 21:23:05 -0600

    vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from "vue-router"
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+import Users from './components/Users.vue'
+const Home = { template: "<p>home page</p>" };
+const Index = { template: "<p>index page</p>" };
+const About = { template: "<p>about page</p>" };
+
+const routes = [
+  { path: "/users", name: "users", component: Users },
+  { path: "/home", name: "home", component: Home },
+  { path: "/index", name: "index", component: Index },
+  { path: "/about", name: "about", component: About },
+];
+
+var router = new VueRouter({
+  routes: routes,
+  mode: "history",
+});
 
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
